ba412e072221ff365693831f4898ccb631d9bf99
Guilhem Smith <gsmith@student.42.fr>
2019-08-13 17:43:28 +0200

    Variable and Function constructor.

--- src/lexer/function.rs
+++ src/lexer/function.rs
@@ -6,7 +6,7 @@
 /*   By: gsmith <gsmith@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/08/13 17:22:09 by gsmith            #+#    #+#             */
-/*   Updated: 2019/08/13 17:32:44 by gsmith           ###   ########.fr       */
+/*   Updated: 2019/08/13 17:42:22 by gsmith           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,24 @@ use std::fmt;
 pub struct Function {
     id: String,
     param: Vec<Variable>,
+}
+
+impl Function {
+    pub fn new(id: String, vars: String) -> Self {
+        let mut new = Function {
+            id: id,
+            param: Vec::new(),
+        };
+        let mut var_name = vars.split_terminator(", ");
+
+        loop {
+            match var_name.next() {
+                Some(name) => new.param.push(Variable::new(String::from(name))),
+                None => break,
+            };
+        }
+        return new;
+    }
 }
 
 impl fmt::Display for Function {
--- src/lexer/variable.rs
+++ src/lexer/variable.rs
@@ -6,7 +6,7 @@
 /*   By: gsmith <gsmith@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/08/13 17:16:26 by gsmith            #+#    #+#             */
-/*   Updated: 2019/08/13 17:27:15 by gsmith           ###   ########.fr       */
+/*   Updated: 2019/08/13 17:36:32 by gsmith           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,12 @@ use std::fmt;
 #[derive(Clone)]
 pub struct Variable {
     id: String,
+}
+
+impl Variable {
+    pub fn new(id: String) -> Self {
+        Variable { id: id }
+    }
 }
 
 impl fmt::Display for Variable {
