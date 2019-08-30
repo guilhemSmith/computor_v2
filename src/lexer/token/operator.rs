9374efec2379b310abd8d89cc810bd49a950b874
Guilhem Smith <smith.guilhem@gmail.com>
2019-08-30 12:31:30 +0200

    Equal can assign variable.

--- src/lexer/token/operator.rs
+++ src/lexer/token/operator.rs
@@ -6,13 +6,14 @@
 /*   By: gsmith <gsmith@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/07/25 17:20:24 by gsmith            #+#    #+#             */
-/*   Updated: 2019/08/21 12:29:49 by gsmith           ###   ########.fr       */
+/*   Updated: 2019/08/30 12:24:25 by gsmith           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 use super::{LexerError, Token};
-use crate::computor::{ComputorError, ComputorResult};
+use crate::computor::{ComputorError as CError, ComputorResult as CResult};
 use crate::memory::{Extension, Memory};
+use crate::types::Imaginary;
 
 use std::any::Any;
 use std::fmt;
@@ -21,7 +22,7 @@ use std::fmt;
 pub struct Operator {
     symbol: char,
     priority: i32,
-    op: fn(&Self, ComputorResult, ComputorResult) -> ComputorResult,
+    op: fn(&Self, CResult, CResult) -> CResult,
 }
 
 impl fmt::Display for Operator {
@@ -49,15 +50,15 @@ impl Token for Operator {
         &self,
         _mem: &Memory,
         _ext: Option<&mut Extension>,
-    ) -> ComputorResult {
-        ComputorResult::Err(ComputorError::unparsed_token(self))
+    ) -> CResult {
+        CResult::Err(CError::unparsed_token(self))
     }
 }
 
 impl Operator {
     pub fn new(symbol: char) -> Result<Self, LexerError> {
         let priority: i32;
-        let op: fn(&Self, ComputorResult, ComputorResult) -> ComputorResult;
+        let op: fn(&Self, CResult, CResult) -> CResult;
         match symbol {
             '=' => {
                 priority = 0;
@@ -99,49 +100,51 @@ impl Operator {
     pub fn exec(
         &self,
         _mem: &Memory,
-        orand_l: ComputorResult,
-        orand_r: ComputorResult,
-    ) -> ComputorResult {
+        orand_l: CResult,
+        orand_r: CResult,
+    ) -> CResult {
         (self.op)(self, orand_l, orand_r)
     }
 
-    fn equal(
-        &self,
-        orand_l: ComputorResult,
-        orand_r: ComputorResult,
-    ) -> ComputorResult {
-        ComputorResult::default()
+    fn equal(&self, orand_l: CResult, orand_r: CResult) -> CResult {
+        match (orand_l, orand_r) {
+            (CResult::Err(err), _) => CResult::Err(err),
+            (_, CResult::Err(err)) => CResult::Err(err),
+            (CResult::None, _) => CResult::Err(CError::bad_use_op(self.symbol)),
+            (_, CResult::None) => CResult::Err(CError::bad_use_op(self.symbol)),
+            (CResult::Val(val), CResult::Res) => CResult::Val(val),
+            (CResult::Var(var, coef, pow), CResult::Res) => {
+                CResult::Var(var, coef, pow)
+            }
+            (CResult::Var(var, coef, pow), CResult::Val(val)) => {
+                let one = Imaginary::new(1.0, 0.0);
+                if pow == one {
+                    if coef == one {
+                        CResult::Set(var, val)
+                    } else {
+                        CResult::default()
+                    }
+                } else {
+                    CResult::default()
+                }
+            }
+            (_, _) => CResult::default(),
+        }
     }
 
-    fn mul(
-        &self,
-        orand_l: ComputorResult,
-        orand_r: ComputorResult,
-    ) -> ComputorResult {
-        ComputorResult::default()
+    fn mul(&self, orand_l: CResult, orand_r: CResult) -> CResult {
+        CResult::default()
     }
 
-    fn div(
-        &self,
-        orand_l: ComputorResult,
-        orand_r: ComputorResult,
-    ) -> ComputorResult {
-        ComputorResult::default()
+    fn div(&self, orand_l: CResult, orand_r: CResult) -> CResult {
+        CResult::default()
     }
 
-    fn add(
-        &self,
-        orand_l: ComputorResult,
-        orand_r: ComputorResult,
-    ) -> ComputorResult {
-        ComputorResult::default()
+    fn add(&self, orand_l: CResult, orand_r: CResult) -> CResult {
+        CResult::default()
     }
 
-    fn sub(
-        &self,
-        orand_l: ComputorResult,
-        orand_r: ComputorResult,
-    ) -> ComputorResult {
-        ComputorResult::default()
+    fn sub(&self, orand_l: CResult, orand_r: CResult) -> CResult {
+        CResult::default()
     }
 }
